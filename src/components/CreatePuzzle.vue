dadb4b1fa860b576268a56c71b03f64384df7139
YutakaNagai <ynagai103@gmail.com>
2024-07-10 01:26:35 +0900

    DB登録機能追加

--- src/components/CreatePuzzle.vue
+++ src/components/CreatePuzzle.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { ref } from "vue";
+import { supabase } from "../supabase";
 
 const dot_field = ref([]);
 for (let i = 0; i < 10; i++) {
   dot_field.value.push(Array(10).fill(false));
 }
+
+const title = ref("");
+const description = ref("");
 
 const paintCell = (c, r) => {
   dot_field.value[c][r] = !dot_field.value[c][r];
@@ -13,43 +17,47 @@ const paintCell = (c, r) => {
 const saveIllust = () => {
   // ヒント作成用関数
   const createHint = (arr) => {
-    const col_nums = [];
-    let hint_str = "";
+    let hint = "";
+    let answer = "";
 
     arr.forEach((child_arr, c_index) => {
       let num_count = 0;
       child_arr.forEach((cell, index) => {
         if (cell) {
+          answer += "1,";
           // 塗られていたらカウントをインクリメント
           num_count++;
           if (index + 1 === child_arr.length) {
             // 列末尾の場合、カウントをプッシュ
-            hint_str += num_count;
+            hint += num_count;
             num_count = 0;
           }
-        } else if (num_count > 0) {
-          // 塗られていない場合、カウントがたまっていればプッシュ
-          hint_str +=
-            index + 1 === child_arr.length ? num_count : `${num_count},`;
-          num_count = 0;
+        } else {
+          answer += "0,";
+          if (num_count > 0) {
+            // 塗られていない場合、カウントがたまっていればプッシュ
+            hint +=
+              index + 1 === child_arr.length ? num_count : `${num_count},`;
+            num_count = 0;
+          }
         }
       });
 
-      if (hint_str.endsWith(",")) {
-        hint_str = hint_str.slice(0, -1);
+      answer = answer.slice(0, -1);
+      if (hint.endsWith(",")) {
+        hint = hint.slice(0, -1);
       }
       // 一行（列）分終わったら区切り記号としてスラッシュを置く
       if (c_index + 1 !== arr.length) {
-        hint_str += "/";
+        hint += "/";
+        answer += "/";
       }
     });
-
-    return hint_str;
+    return { hint, answer };
   };
 
   // 垂直方向のヒント作成
-  const col_hint = createHint(dot_field.value);
-  console.log("col_hint :>> ", col_hint);
+  const col = createHint(dot_field.value);
 
   // 二次元配列を転置
   const transpose_array = dot_field.value[0].map((_, c) =>
@@ -57,8 +65,55 @@ const saveIllust = () => {
   );
 
   // 水平方向のヒント作成
-  const row_hint = createHint(transpose_array);
-  console.log("row_hint :>> ", row_hint);
+  const row = createHint(transpose_array);
+
+  addPuzzle(
+    title.value,
+    description.value,
+    "0",
+    "テストユーザー",
+    col.answer,
+    col.hint,
+    row.answer,
+    row.hint,
+    dot_field.value.length,
+    dot_field.value[0].length
+  );
+};
+
+const addPuzzle = async (
+  title,
+  description,
+  creator_id,
+  creator_name,
+  col_answer,
+  col_hint,
+  row_answer,
+  row_hint,
+  num_of_col,
+  num_of_row
+) => {
+  const { data, error } = await supabase.from("illust_logic").insert([
+    {
+      title,
+      description,
+      creator_id,
+      creator_name,
+      col_answer,
+      col_hint,
+      row_answer,
+      row_hint,
+      num_of_col,
+      num_of_row,
+      challenged: 0,
+      solved: 0,
+      good: 0,
+      bad: 0,
+      is_deleted: false,
+    },
+  ]);
+  console.log("data :>> ", data);
+  console.log("error :>> ", error);
 };
 
 const initIllust = () => {
@@ -92,6 +147,17 @@ const initIllust = () => {
       </div>
     </div>
 
+    <div class="input_area">
+      <div class="input_set">
+        <span>タイトル: </span>
+        <input type="text" v-model="title" />
+      </div>
+      <div class="input_set">
+        <span>詳細: </span>
+        <input type="text" v-model="description" />
+      </div>
+    </div>
+
     <div class="btn_area">
       <button @click="initIllust()">クリア</button>
       <button @click="saveIllust()">保存</button>
@@ -102,7 +168,8 @@ const initIllust = () => {
 <style scoped>
 .illust_field {
   display: flex;
-  margin-bottom: 100px;
+  justify-content: center;
+  margin: 25px;
 }
 .cell {
   width: 20px;
@@ -114,8 +181,20 @@ const initIllust = () => {
 .painted {
   background-color: rgb(104, 98, 98);
 }
-.btn_area {
+.input_area {
+  margin: 25px;
+  display: flex;
+  flex-flow: column;
+  justify-content: space-evenly;
+}
+.input_set {
+  margin: 5px;
+  width: 100%;
   display: flex;
   justify-content: space-between;
+}
+.btn_area {
+  display: flex;
+  justify-content: space-evenly;
 }
 </style>
